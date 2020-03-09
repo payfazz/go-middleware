d5013f27bce444a7276236a5d707b6490eefdfb1
Kurnia D Win <kurnia.d.win@gmail.com>
2020-03-09 23:08:25 +0700

    adding adapter func for printer

--- util/printer/printer.go
+++ util/printer/printer.go
@@ -5,3 +5,11 @@ package printer
 type Printer interface {
 	Print(v ...interface{})
 }
+
+// Func is type adapter for Printer interface
+type Func func(v ...interface{})
+
+// Print call f(v...)
+func (f Func) Print(v ...interface{}) {
+	f(v...)
+}
